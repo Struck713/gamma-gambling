1d9174abfed0bd5970b68a78f4720babfb7cac5c
Struck713 <noahstruck71303@gmail.com>
2023-03-23 22:28:44 -0400

    Fixed ESLint error (fine Vercel I will fix it)

--- website/src/pages/account/login.tsx
+++ website/src/pages/account/login.tsx
@@ -10,7 +10,7 @@ const AccountLogin = () => {
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+              We&apos;ll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
 
