ae158921fa438eaf64b5fa30cd8a1f60addffc71
novano1d <mmoor179@kent.edu>
2023-04-04 20:40:58 -0400

    implemented p5

--- website/src/pages/games/slots.tsx
+++ website/src/pages/games/slots.tsx
@@ -1,8 +1,31 @@
-
+import React, { ComponentProps } from "react";
+import Sketch from "react-p5";
+import p5Types from "p5";
+import { clear } from "console";
 // The roulette game
+
+// See annotations in JS for more information
+const setup = (p5: p5Types, canvasParentRef: Element) => {
+  p5.createCanvas(600, 700).parent(canvasParentRef);
+  p5.background(0,0,0,0);
+};
+
+let x = 50;
+const y = 50;
+const draw = (p5: p5Types) => {
+  p5.ellipse(x, y, 70, 70);
+  x++;
+};
+
 const Slots = () => {
-    return (
-      <></>
+    return(
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }}>
+        <Sketch setup={setup} draw={draw} />
+    </div>
     )
   }
   
