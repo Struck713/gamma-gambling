3b775762ddbf5e4d52e9419d0b91ee18cf0eb0c9
Struck713 <noahstruck71303@gmail.com>
2023-04-27 23:45:54 -0400

    Fixed no 404 error on games page

--- website/src/pages/404.tsx
+++ website/src/pages/404.tsx
@@ -1,11 +1,12 @@
-import { Alert } from 'react-bootstrap';
-
 // Ummm... it&apos;s telling me this page doesn&apos;t exist..?
 const Error = () => {
   return (
     <>
-      <div style={{ margin: '1rem' }}>
-        <h1 className="light text-center align-middle text-light">404</h1>
+      <div className="jumbotron text-light text-center">
+        <hgroup>
+          <h1>404</h1>
+          <h2>Ummm... it&apos;s telling me this page doesn&apos;t exist..?</h2>
+        </hgroup>
       </div>
     </>
   )
